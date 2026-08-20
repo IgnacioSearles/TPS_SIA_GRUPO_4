502ede79fc9ac9dc16aba6992e221ccc9a8c0e30
Nicolas Koron <nkoron@itba.edu.ar>
2026-08-20 15:39:24 -0300

    Revert "Log detected dead squares"
    This reverts commit 4908ac4deb533f85eb2e791e191acd27006dee83.

--- TP1/state.py
+++ TP1/state.py
@@ -1,12 +1,10 @@
 from collections import deque
 from functools import lru_cache
-import logging
 from typing import List, FrozenSet, Tuple, Optional
 from enum import Enum
 from dataclasses import dataclass
 
 PRUNING_MODES = {"dead_squares", "local"}
-logger = logging.getLogger(__name__)
 
 class Action(Enum):
     UP = ("U", 0, -1)
@@ -66,12 +64,7 @@ def _dead_squares(
             reachable.add(predecessor)
             frontier.append(predecessor)
 
-    dead_squares = frozenset(floor - reachable - goals)
-    logger.info(
-        "Dead squares detected: %s",
-        sorted(dead_squares),
-    )
-    return dead_squares
+    return frozenset(floor - reachable - goals)
 
 
 class State:
